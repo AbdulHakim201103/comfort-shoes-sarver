248b1c89622bc3173bf0278701ab7f7e2d049434
AbdulHakim201103 <abdulhakim201103@gmail.com>
2022-06-01 23:49:27 +0600

    test

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run() {
       res.send(result);
     });
 
-    //  findOne
+    //  findOnes
     app.get("/inventory/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
